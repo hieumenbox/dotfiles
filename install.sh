e37358f1e75def14aadb4cf44059047d47201d38
Hieu Nguyen <hieu.nguyen.dn@renesas.com>
2024-05-06 19:04:07 +0700

    add(nvim): Add link to local share data

--- install.sh
+++ install.sh
@@ -42,6 +42,10 @@ ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
 ln -sf "$PWD/vim/.vimrc" "$HOME"/.vimrc
 ln -sf "$PWD/ssh/config" "$HOME"/.ssh/config
 
+# Symbolic link local share data
+rm -rf ~/.local/share/nvim
+ln -s /shsv/Android/SoftIP/35_hieunguyen/setup_env/plugin/nvim_data ~/.local/share/nvim
+
 # set up blog
 # git clone git@github.com:mischavandenburg/hugo-PaperModX-theme.git themes/PaperModX --depth=1
 
